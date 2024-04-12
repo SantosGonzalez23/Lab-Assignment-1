55ad19fcd6e88d5174b2fb0ce6398a174fe324c3
Santos Gonzalez <santos.gonzalez@cwu.edu>
2024-04-11 21:34:26 -0700

    Implemented preOrderTraversal method

--- TreeDemo.java
+++ TreeDemo.java
@@ -57,11 +57,13 @@ class Node{
 	   pre-order traversal
 	   Prints the value of every node preorder
 	   */
-	   public void preOrderTraversal(Node root){
-		//implement in here
-		   
-	   }
-
+	   public void preOrderTraversal(Node root) {
+        if (root != null) {
+            System.out.print(root.value + " ");
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+    }
 	   
 	   
 	   /*
